af41c4e967a463ec22fe282edbcfa2f40093158f
VerZsuT <verzsut@gmail.com>
2022-07-26 15:54:13 +0300

    Изменение работы setState
    - Объект изначального состояние больше не мутируется.
    - Добавлено поверхностное сравнение предыдущего и нового состояния.

--- src/prepareState.ts
+++ src/prepareState.ts
@@ -3,14 +3,19 @@
 import { useState } from 'react'
 
 export default <T extends {}>(initial: T) => {
-    const state = initial
-    const setState = useState({})[1]
+    const state = {...initial}
+    const setState = useState(null)[1]
 
     return [
         state,
         partialState => {
-            Object.assign(state, partialState)
-            setState({})
+            for (const name in partialState) {
+                if (state[name] !== partialState[name]) {
+                    Object.assign(state, partialState)
+                    setState({})
+                    break
+                }
+            }
         }
     ] as [T, (partialState: Partial<T>) => void]
 }

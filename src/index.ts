c4a0338768179a20d6a04facecd5280d0457ab0b
VerZsuT <verzsut@gmail.com>
2022-08-20 14:31:21 +0300

    Оптимизация

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,54 @@
-import { memo, useContext, useEffect, useRef, useState, useLayoutEffect, useMemo } from 'react'
-import type { Context, FC } from 'react'
+import {memo, useContext, useEffect, useRef, useState, useLayoutEffect, useMemo} from 'react'
+import type {Context, FC} from 'react'
 
-import { useDispatch, useSelector } from 'react-redux'
-import type { AnyAction, Dispatch } from 'redux'
+import {useDispatch, useSelector} from 'react-redux'
+import type {AnyAction, Dispatch} from 'redux'
 
-import type { Constructor, Data, UseReduxConfig } from './types'
+import type {Constructor, Data, UseReduxConfig} from './types'
 
-let currentData: Data<any> = null
+let currentData: Data<any> = {
+    set beforeRender(_) {
+        throw new Error('Attempt to outside call constructor\'s method')
+    },
+    get beforeRender(): () => void {
+        throw new Error('Attempt to outside call constructor\'s method')
+    },
+    render: null,
+    props: {}
+}
+
 
 /**
  * Returns a component with constructor functionality
  */
 export function afc<P extends {}>(constructor: Constructor<P>): FC<P> {
     return (props: P) => {
-        const data = useData<P>()
-        updateProps(data.props, props)
+        const ref = useRef<Data<P>>()
+        let data = ref.current
 
-        if (data.render)
-            data.beforeRender()
-        else
-            construct(constructor, data)
+        if (!data) {
+            ref.current = data = {
+                beforeRender() {
+                    const prevData = currentData
+                    data.beforeRender = null
+                    currentData = data
+                    data.render = constructor(data.props)
+                    currentData = prevData
+                },
+                render: null,
+                props: <P>{}
+            }
+        }
 
+        for (const key in data.props) {
+            if (!Object.hasOwn(props, key))
+                delete data.props[key]
+        }
+
+        for (const key in props)
+            data.props[key] = props[key]
+
+        data.beforeRender()
         return data.render()
     }
 }
@@ -36,9 +64,8 @@ export function afcMemo<P extends {}>(constructor: Constructor<P>) {
  * Returns the getter of the memoized value
  */
 export function memoized<T>(factory: () => T, depsGetter: () => any[]) {
-    checkOutsideCall('memoized')
     let value: T
-    callAndAddToRender(() => {
+    addToRenderAndCall(() => {
         value = useMemo<T>(factory, depsGetter())
     })
 
@@ -49,8 +76,7 @@ export function memoized<T>(factory: () => T, depsGetter: () => any[]) {
  * Returns redux-dispatcher
  */
 export function getDispatcher<T extends Dispatch<AnyAction>>(): T {
-    checkOutsideCall('getDispatcher')
-    return callAndAddToRender<T>(useDispatch)
+    return addToRenderAndCall<T>(useDispatch)
 }
 
 /**
@@ -58,7 +84,6 @@ export function getDispatcher<T extends Dispatch<AnyAction>>(): T {
  * @returns context getter
  */
 export function handleContext<T>(context: Context<T>): () => T {
-    checkOutsideCall('handleContext')
     let contextValue = useContext(context)
     addToRender(() => {
         contextValue = useContext(context)
@@ -72,8 +97,7 @@ export function handleContext<T>(context: Context<T>): () => T {
  * _Analog of `useEffect(() => callback, [])`_
  */
 export function afterUnmount(callback: () => void): void {
-    checkOutsideCall('afterUnmount')
-    callAndAddToRender(() => {
+    addToRenderAndCall(() => {
         useEffect(() => callback, [])
     })
 }
@@ -84,8 +108,7 @@ export function afterUnmount(callback: () => void): void {
  * _Analog of `useEffect(callback, [])`_
  */
 export function afterMount(callback: () => void): void {
-    checkOutsideCall('afterMount')
-    callAndAddToRender(() => {
+    addToRenderAndCall(() => {
         useEffect(callback, [])
     })
 }
@@ -96,8 +119,7 @@ export function afterMount(callback: () => void): void {
  _Analog of `useLayoutEffect(callback, [])`_
  */
 export function afterDraw(callback: () => void): void {
-    checkOutsideCall('afterDraw')
-    callAndAddToRender(() => {
+    addToRenderAndCall(() => {
         useLayoutEffect(callback, [])
     })
 }
@@ -109,7 +131,6 @@ export function afterDraw(callback: () => void): void {
  * @returns - [state, stateGetter]
  */
 export function createState<T>(initial: T) {
-    checkOutsideCall('createState')
     addToRender(() => useState(null))
     return prepareState(initial)
 }
@@ -118,8 +139,7 @@ export function createState<T>(initial: T) {
  * Calls the function immediately and before each render
  */
 export function inRender(callback: () => void): void {
-    checkOutsideCall('inRender')
-    callAndAddToRender(callback)
+    addToRenderAndCall(callback)
 }
 
 /**
@@ -129,10 +149,9 @@ export function inRender(callback: () => void): void {
 export function useRedux<T extends UseReduxConfig<any>>(config: T): {
     [key in keyof typeof config]: ReturnType<typeof config[key]>
 } {
-    checkOutsideCall('useRedux')
     const state: any = {}
 
-    callAndAddToRender(() => {
+    addToRenderAndCall(() => {
         for (const name in config)
             state[name] = useSelector(config[name])
     })
@@ -143,63 +162,31 @@ export function useRedux<T extends UseReduxConfig<any>>(config: T): {
 function addToRender(callback: () => void): void {
     const prev = currentData.beforeRender
     currentData.beforeRender = () => {
-        prev()
+        prev?.()
         callback()
     }
 }
 
-function callAndAddToRender<T>(callback: () => T): T {
+function addToRenderAndCall<T>(callback: () => T): T {
     addToRender(callback)
     return callback()
 }
 
 function prepareState<T extends {}>(initial: T): [T, (partialState: Partial<T>) => void] {
     const state = {...initial}
-    const setState = useState(null)[1]
+    const setState = useState<{}>()[1]
 
     return [
         state,
         partialState => {
             for (const name in partialState) {
                 if (state[name] !== partialState[name]) {
-                    Object.assign(state, partialState)
+                    for (const key in partialState)
+                        state[key] = partialState[key]
                     setState({})
                     break
                 }
             }
         }
     ]
-}
-
-function checkOutsideCall(name: string): void {
-    if (currentData === null)
-        throw new Error(`Attempt to call '${name}' outside the constructor`)
-}
-
-function construct<P extends {}>(constructor: Constructor<P>, data: Data<P>): void {
-    const prevData = currentData
-    currentData = data
-    data.render = constructor(data.props)
-    currentData = prevData
-}
-
-function updateProps<P extends {}>(prevProps: P, newProps: P): void {
-    for (const name in prevProps)
-        delete prevProps[name]
-
-    Object.assign(prevProps, newProps)
-}
-
-function useData<P extends {}>(): Data<P> {
-    const ref = useRef<Data<P>>(null)
-
-    if (!ref.current) {
-        ref.current = {
-            beforeRender: () => null,
-            render: null,
-            props: <P>{}
-        }
-    }
-
-    return ref.current
 }
